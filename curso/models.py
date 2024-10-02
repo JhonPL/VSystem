b67feb3c10647f9c19ac95913868c96e1343ff30
Jhon Paternina <jhonpaternina2002@gmail.com>
2024-10-01 19:20:42 -0500

    Agregado alerta al agregar curso no se pueda un estudiante -OK

--- curso/models.py
+++ curso/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 from persona.models import Persona
+from django.core.exceptions import ValidationError
 
 class Curso(models.Model):
     nombre = models.CharField(max_length=100)
     profesor = models.ForeignKey(Persona, on_delete=models.CASCADE) 
     create_at = models.DateTimeField(auto_now=True)
 
+    def clean(self):
+        
+        if self.profesor.rol != 'Profesor':
+            raise ValidationError(f'{self.profesor.nombre} {self.profesor.apellidos} no tiene el rol de Profesor.')
+
     def __str__(self):
-        return f'{self.id} {self.nombre} - {self.profesor.nombre} {self.profesor.apellidos}'
+        return f' {self.nombre} - {self.profesor.nombre} {self.profesor.apellidos}'
 
     class Meta:
         db_table = 'Curso'
