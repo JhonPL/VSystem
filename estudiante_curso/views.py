95fdb9b4a33a5d96b2b7236c23121b3ada5cbf1d
Jhon Paternina <jhonpaternina2002@gmail.com>
2024-10-14 14:03:54 -0500

    nuevos filtros estudiante-curso -OK

--- estudiante_curso/views.py
+++ estudiante_curso/views.py
@@ -4,10 +4,35 @@ from .forms import EstudianteCursoForm
 
 # Vista para listar los estudiantes y cursos
 def Estudiante_Curso(request):
-    estudianteCurso = EstudianteCurso.objects.all()  # Obtener todos los objetos EstudianteCurso
+     # Obtener los valores del filtro de búsqueda
+    estudiante = request.GET.get('estudiante')
+    curso = request.GET.get('curso')
+    estado = request.GET.get('estado')
+
+    # Obtener todos los registros de la relación EstudianteCurso
+    estudianteCurso = EstudianteCurso.objects.all()
+
+    # Filtrar por nombre o apellidos de estudiante si hay búsqueda
+    if estudiante:
+        estudianteCurso = estudianteCurso.filter(estudiante__nombre__icontains=estudiante) | estudianteCurso.filter(estudiante__apellidos__icontains=estudiante)
+
+    # Filtrar por curso si se selecciona uno
+    if curso:
+        estudianteCurso = estudianteCurso.filter(curso__id=curso)
+
+    # Filtrar por estado si se selecciona uno
+    if estado:
+        estudianteCurso = estudianteCurso.filter(estado=estado)
+
+    # Obtener todas las opciones para los desplegables
+    cursos = EstudianteCurso.objects.values('curso__id', 'curso__nombre').distinct()
+    estados = EstudianteCurso.objects.values('estado').distinct()
+
     return render(request, 'lista_est_cur.html', {
         'title': 'Relación estudiantes y curso',
         'estudiantes_cursos': estudianteCurso,
+        'cursos': cursos,
+        'estados': estados,
     })
 
 # Vista para agregar o editar una relación de estudiante con curso
